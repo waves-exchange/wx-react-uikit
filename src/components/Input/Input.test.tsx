47668fd54074803187fbf0eac2f6c657ffed9bba
Vusal27 <38328237+Vusal27@users.noreply.github.com>
2022-09-01 13:51:39 +0300

    DEXW-5612: update input error color (#7)

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -18,7 +18,7 @@ describe('Input', () => {
                     $0: 'white',
                 },
                 danger: {
-                    $600: 'red',
+                    $300: 'red',
                 },
             },
         };

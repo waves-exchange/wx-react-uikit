4a5b5b81b93eff1fc6cc231c6fb7b0c826e3cc46
Maxim Malyavin <mmalyavin@web3tech.ru>
2022-09-19 18:12:15 +0300

    DEXW-5713: add accordion and his update

---
+++ src/icons/expandAccordion.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const iconExpandAccordion = {
+    path: (
+        <>
+            <path d="M14 5L8 11L2 5" stroke="currentColor" />
+        </>
+    ),
+    viewBox: '0 0 16 16',
+};

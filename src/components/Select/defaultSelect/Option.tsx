d2a4def29137aa34df4402db1b599de85c4347bf
Max <57911146+max-malyavin@users.noreply.github.com>
2022-09-21 10:58:01 +0300

    DEXW-5710: update select (#20)
    * DEXW-5710: update select
    
    * DEXW-5710: some refactor
    
    * DEXW-5710: fix
    
    * DEXW-5710: fix DefaultOption interface
    
    Co-authored-by: Maxim Malyavin <mmalyavin@web3tech.ru>

---
+++ src/components/Select/defaultSelect/Option.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Flex, TFlexProps } from '../../Flex/Flex';
+
+export type DefaultOption = {
+    id: string;
+    value?: () => React.ReactNode | React.ReactNode;
+    renderPlaceholder?: () => React.ReactNode;
+};
+
+export const Option: React.FC<TFlexProps & { option: DefaultOption }> = ({
+    option: { value, renderPlaceholder },
+    ...rest
+}) => {
+    return (
+        <Flex
+            justifyContent="space-between"
+            flex={1}
+            fontSize="$14"
+            lineHeight="$24"
+            {...rest}
+        >
+            {value
+                ? typeof value === 'function'
+                    ? value()
+                    : value
+                : renderPlaceholder
+                ? renderPlaceholder()
+                : ''}
+        </Flex>
+    );
+};

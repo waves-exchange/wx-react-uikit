ec3a674c9351e3b1486309b4d036845de47f72f0
Olya <olyakanischeva.va@gmail.com>
2023-02-17 17:20:38 +0400

    add AssetLogo component (#41)
    * add AssetLogo component
    
    * 1.3.10
    
    * fix color icon
    
    * 1.3.11
    
    ---------
    
    Co-authored-by: Maxim Malyavin <mmalyavin@web3tech.ru>

---
+++ src/components/AssetLogo/AssetLogoWithIcon.tsx
@@ -0,0 +1,94 @@
+import { AssetLogo, AssetLogoProps } from './AssetLogo';
+import { Box, BoxProps } from '../Box/Box';
+import { IIcon, Icon } from '../Icon/Icon';
+import React, { FC, ReactNode } from 'react';
+import { Flex } from '../Flex/Flex';
+import { Options } from '@popperjs/core';
+import { Text } from '../Text/Text';
+import { Tooltip } from '../Tooltip/Tooltip';
+
+type AssetLogoWithIconProps = BoxProps &
+    AssetLogoProps & {
+        icon: IIcon;
+        iconLabel: string;
+        iconVisible: boolean;
+        popperOptions: Partial<Options>;
+    };
+
+export const AssetLogoWithIcon: FC<AssetLogoWithIconProps> = ({
+    assetId,
+    name,
+    logo,
+    variant,
+    popperOptions,
+    icon,
+    iconLabel,
+    iconVisible,
+    size,
+    ...rest
+}) => {
+    return (
+        <Box position="relative" {...rest}>
+            <AssetLogo
+                assetId={assetId}
+                name={name}
+                logo={logo}
+                variant={variant}
+                size={size}
+            />
+            {iconVisible && (
+                <Tooltip
+                    variant="info"
+                    label={(): ReactNode => (
+                        <Flex
+                            color="text"
+                            width="max-content"
+                            maxWidth="100%"
+                            alignItems="center"
+                        >
+                            <Flex
+                                alignItems="center"
+                                justifyContent="center"
+                                size="16px"
+                                borderRadius="circle"
+                                backgroundColor="surf"
+                                flex="none"
+                            >
+                                <Icon icon={icon} size={10} color="text" />
+                            </Flex>
+                            <Text ml="$8" variant="body2">
+                                {iconLabel}
+                            </Text>
+                        </Flex>
+                    )}
+                    popperOptions={{
+                        modifiers: [
+                            {
+                                name: 'flip',
+                                enabled: false,
+                            },
+                        ],
+                        strategy: 'fixed',
+                        ...popperOptions,
+                    }}
+                >
+                    <Flex
+                        position="absolute"
+                        bottom={-2}
+                        right={3}
+                        width={14}
+                        height={14}
+                        justifyContent="center"
+                        alignItems="center"
+                        borderRadius="circle"
+                        backgroundColor="surf"
+                        cursor="pointer"
+                        zIndex={2}
+                    >
+                        <Icon icon={icon} size={8} color="text" />
+                    </Flex>
+                </Tooltip>
+            )}
+        </Box>
+    );
+};

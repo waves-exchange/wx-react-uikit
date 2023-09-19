db486fe234f4e6822c787a5495569ce4c017c2c7
vgakhramanov <vgakhramanov@web3tech.ru>
2023-09-19 16:58:36 +0300

    DEXW-6638: some icons were updated

--- src/icons/chevron.tsx
+++ src/icons/chevron.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export const chevron = {
-    path: <path d="M6 9L12 15L18 9" stroke="currentColor" fill="none" />,
+    path: (
+        <path
+            d="M18.0703 10L16.5703 8.5L12 13.0703L7.42969 8.5L5.92969 10L12 16.0703L18.0703 10Z"
+            fill="currentColor"
+        />
+    ),
     viewBox: '0 0 24 24',
 };

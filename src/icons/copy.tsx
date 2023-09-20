f1f0a2c68ed62d7e08e70dc404297ab1254c3006
Vusal27 <38328237+Vusal27@users.noreply.github.com>
2023-09-20 14:31:13 +0300

    DEXW-6638: some icons were updated (#55)
    * DEXW-6638: some icons were updated
    
    * 1.8.0

--- src/icons/copy.tsx
+++ src/icons/copy.tsx
@@ -2,20 +2,10 @@ import React from 'react';
 
 export const iconCopy = {
     path: (
-        <>
-            <path
-                d="M5.5 5.5L14.5 5.5V14.5H5.5V5.5Z"
-                stroke="currentColor"
-                fill="transparent"
-                strokeWidth="1.2"
-            />
-            <path
-                d="M10.5 4V1.5H1.5V10.5H4"
-                stroke="currentColor"
-                fill="transparent"
-                strokeWidth="1.2"
-            />
-        </>
+        <path
+            d="M4 2C2.895 2 2 2.895 2 4V17C2 17.552 2.448 18 3 18C3.552 18 4 17.552 4 17V4H17C17.552 4 18 3.552 18 3C18 2.448 17.552 2 17 2H4ZM8 6C6.895 6 6 6.895 6 8V20C6 21.105 6.895 22 8 22H20C21.105 22 22 21.105 22 20V8C22 6.895 21.105 6 20 6H8ZM8 8H20V20H8V8Z"
+            fill="currentColor"
+        />
     ),
-    viewBox: '0 0 16 16',
+    viewBox: '0 0 24 24',
 };
